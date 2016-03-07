da98830950200f8b1b7df10e3be515a9b97d5f5d
Gibberish314 <a.moosen@gmail.com>
2016-03-07 01:01:51 -0800

    Fix timing on index.html and add Task model with display of one task object.

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,10 +4,21 @@ import { Component } from 'angular2/core';
   selector: 'my-app',
   template: `
     <div class="container">
-      <h1>Skeleton Angular2 App!</h1>
+      <h1>To-Do List</h1>
+      <h3>{{ task.description }}<h3>
     <div>
   `
 })
 export class AppComponent { 
+  public task: Task;
+  constructor(){
+    this.task = new Task("Create To-Do List app.", 0);
+  }
+}
 
+export class Task {
+  public done: boolean = false;
+  constructor(public description: string, public id: number) {
+
+  }
 }
