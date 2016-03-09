bee330519e2d7504f6274ccd7d10dad1280cbf04
Staff <staff@staff-7.local>
2016-03-09 11:51:50 -0800

    Instantiate edit description form in TaskListComponent.

---
+++ app/new-task.component.ts
@@ -0,0 +1,12 @@
+import {Component} from 'angular2/core';
+import {Task} from './task.model';
+
+@Component({
+  selector: 'new-task',
+  template: `
+    <h3>Blank template</h3>
+  `
+})
+export class NewTaskComponent {
+
+}
